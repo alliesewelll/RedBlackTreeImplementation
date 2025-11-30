97410c43b018bed0eb31e5e66521810b0373052b
Allie <alliesewell@gmail.com>
2025-11-30 00:25:07 -0500

    Created test file

--- RedBlackTreeImplementation.java
+++ RedBlackTreeImplementation.java
@@ -16,27 +16,121 @@ class RedBlackTree{
             this.data = data;
             this.color = Color.RED; // New node is red by default
             left = right = parent = null;
+            left.color = right.color = Color.BLACK; // NIL children are black
         }
     }
 
 
     RedBlackTree() {
-        root = null;
-        root.color = "BLACK"; // Root is always black
+        NIL = new RBNode(0);
+        NIL.left = NIL.right = NIL.parent = null;
+        NIL.color = Color.BLACK; // Root is always black
+        root = NIL;
     }
 
-    void insert(int data) {
-        root = insertRec(root, data);
+    public void insert(int data) {
+        RBNode newNode = new RBNode(data);
+        newNode.left = newNode.right = NIL;
+        bstInsert(newNode);
+        fixInsert(newNode); 
     }
 
-    Node insertRec(Node root, int data) {
-        if (root == null) {
-            root = new Node(data);
-            return root;
+    public void bstInsert(RBNode newNode) {
+        RBNode y = NIL;
+        RBNode x = root;
+
+        while (x != NIL) {
+            y = x;
+            if (newNode.data < x.data) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
         }
-        if (data < root.data)
-            root.left = insertRec(root.left, data);
-        else if (data > root.data)
-            root.right = insertRec(root.right, data);
-        
+
+        newNode.parent = y;
+        if (y == NIL) {
+            root = newNode; // Tree was empty
+        } else if (newNode.data < y.data) {
+            y.left = newNode;
+        } else {
+            y.right = newNode;
+        }
+    }
+
+    public void fixInsert(RBNode z) {
+        while (z.parent.color == Color.RED) {
+            if (z.parent == z.parent.parent.left) {
+                RBNode y = z.parent.parent.right; // Uncle
+                if (y.color == Color.RED) {
+                    z.parent.color = Color.BLACK;
+                    y.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    z = z.parent.parent;
+                } else {
+                    if (z == z.parent.right) {
+                        z = z.parent;
+                        leftRotate(z);
+                    }
+                    z.parent.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    rightRotate(z.parent.parent);
+                }
+            } else {
+                RBNode y = z.parent.parent.left; // Uncle
+                if (y.color == Color.RED) {
+                    z.parent.color = Color.BLACK;
+                    y.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    z = z.parent.parent;
+                } else {
+                    if (z == z.parent.left) {
+                        z = z.parent;
+                        rightRotate(z);
+                    }
+                    z.parent.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    leftRotate(z.parent.parent);
+                }
+            }
+        }
+        root.color = Color.BLACK; // Ensure root is always black
+    }
+
+    public void leftRotate(RBNode x) {
+        RBNode y = x.right;
+        x.right = y.left;
+        if (y.left != NIL) {
+            y.left.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == NIL) {
+            root = y;
+        } else if (x == x.parent.left) {
+            x.parent.left = y;
+        } else {
+            x.parent.right = y;
+        }
+        y.left = x;
+        x.parent = y;
+    }
+
+    public void rightRotate(RBNode y) {
+        RBNode x = y.left;
+        y.left = x.right;
+        if (x.right != NIL) {
+            x.right.parent = y;
+        }
+        x.parent = y.parent;
+        if (y.parent == NIL) {
+            root = x;
+        } else if (y == y.parent.right) {
+            y.parent.right = x;
+        } else {
+            y.parent.left = x;
+        }
+        x.right = y;
+        y.parent = x;
+    }
+    
 }
---
+++ RedBlackTreeTest.java
@@ -0,0 +1,6 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+public class RedBlackTreeTest {
+    
+}
